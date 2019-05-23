d31fedd15dd049b02d0f818e5ecbbf7811712e25
Bruno Ascenca <brunoascenc1@outlook.com>
2019-05-23 12:46:23 -0300

    react basics

--- as/src/App.js
+++ as/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Tweet from './tweet'
 //import Hello from './sayHello' Importa o codigo
 // Tudo que foi escrito em sayHello() está nesse <hello/> tag
 function App(){
-  
+
+  // const [isPurple, setPurple] = useState(false)
+  // const [count, setCount] = useState(0)
+
+  // const increment = () => {
+  //   setCount(count + 1)
+  //   setPurple(!isPurple)
+  // }
+
+  // Api example
+  const [users, setUsers] = useState ([
+    {name: 'Bruno', message: 'Hello World!'},
+    {name: 'John', message: 'React is awesome!'},
+    {name: 'Louis', message: 'Hello there'}
+  ])
+
   return(
     <div className="app">
-      <h1>Hello World</h1>
-      <Tweet/>
+      {/* <Tweet name="test Name" message = "message1"/>
+      <Tweet name="test Name2" message = "message2"/>
+      <Tweet name="test Name3" message = "message3"/>
+      <Tweet name="test Name4" message = "message4"/> */}
+      {/* <h1 className={isPurple ? 'purple' : ''}>Mudar cor</h1>
+      <button onClick = {increment}>Contar</button>
+      <h1>{count}</h1> */}
+
+      {users.map(user => (
+        <Tweet name={user.name} message = {user.message}/>
+      ))}
     </div>
   );
 }
