64aa65da7a1353aa81a335c92f9eab4847afa879
Bruno <brunoascenc1@outlook.com>
2020-03-25 22:01:10 -0300

    smooth nav

--- rocket/main.js
+++ rocket/main.js
@@ -1,3 +1,4 @@
+//Responsive Nav
 const navShow = () => {
     const burguer = document.querySelector('.burguer');
     const nav = document.querySelector('.links');
@@ -28,3 +29,51 @@ window.addEventListener('scroll', () =>{
     var header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+
+//Navbar Animation
+const sections = document.querySelectorAll('section')
+const bubble = document.querySelector('.bubble')
+const gradients = ['rgb(199, 111, 240)']
+
+const options = {
+    threshold: 0.7
+}
+
+let observer = new IntersectionObserver(navCheck, options)
+
+function navCheck(entries){
+    entries.forEach(entry => {
+        const className = entry.target.className
+        const activeAnchor = document.querySelector(`[data-page=${className}]`)
+        const gradientIndex = entry.target.getAttribute('data-index')
+        const coords = activeAnchor.getBoundingClientRect()
+        console.log(className)
+        const directions = {
+            height: coords.height,
+            width: coords.width,
+            top: coords.top,
+            left: coords.left
+        }
+        if(entry.isIntersecting){
+            bubble.style.setProperty('left', `${directions.left}px`)
+            bubble.style.setProperty('top', `${directions.top}px`)
+            bubble.style.setProperty('width', `${directions.width}px`)
+            bubble.style.setProperty('height', `${directions.height}px`)
+            bubble.style.background = gradients[gradientIndex]
+            activeAnchor.classList.toggle('activeLink')
+        }else{
+            activeAnchor.classList.remove('activeLink')
+        }
+
+    })
+
+}
+
+sections.forEach(section => {
+    observer.observe(section)
+})
+
+const scroll = new SmoothScroll('.links a[href*="#"]', {
+    speed: 600
+})
