48b0f92d70ad1cbb59a9f886f72c6c60fb18b578
Bruno <brunoascenc1@outlook.com>
2020-05-04 12:35:02 -0300

    responsive

--- portf/js/main.js
+++ portf/js/main.js
@@ -1,14 +1,37 @@
-const cursor = document.querySelector('.cursor');
+//Cursor custom
+const cursor = document.querySelector(".cursor");
 
-document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
 
-})
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
 
-document.addEventListener('click', () => {
-    cursor.classList.add("expand");
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
 
-    setTimeout(() => {
-        cursor.classList.remove("expand");
-    }, 500)
-})
+//Responsive Nav
+const navShow = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".links");
+
+  burger.addEventListener("click", () => {
+    //toggle nav
+    nav.classList.toggle("nav-active");
+    //Burger animation
+    burger.classList.toggle("burgerToggle");
+  });
+};
+navShow();
+
+//Fixed Nav
+window.addEventListener("scroll", () => {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
