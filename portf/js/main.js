ccea0111292fa71241f37590312f4d0517406426
Bruno <brunoascenc1@outlook.com>
2020-05-05 11:21:34 -0300

    animation

--- portf/js/main.js
+++ portf/js/main.js
@@ -1,5 +1,6 @@
 //Cursor custom
 const cursor = document.querySelector(".cursor");
+const navLinks = document.querySelectorAll(".links li");
 
 document.addEventListener("mousemove", (e) => {
   cursor.setAttribute(
@@ -7,6 +8,17 @@ document.addEventListener("mousemove", (e) => {
     "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
   );
 });
+
+navLinks.forEach(link => {
+  link.addEventListener("mouseleave", () => {
+    cursor.classList.remove("cursor-grow");
+    link.classList.remove("hover-link");
+  })
+  link.addEventListener("mouseover", () => {
+    cursor.classList.add("cursor-grow");
+    link.classList.add("hover-link");
+  })
+})
 
 document.addEventListener("click", () => {
   cursor.classList.add("expand");
@@ -30,8 +42,72 @@ const navShow = () => {
 };
 navShow();
 
+const closeNav = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".links");
+  // const navLinks = document.querySelectorAll(".links li");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("nav-active");
+      burger.classList.toggle("burgerToggle")
+    })
+
+  })
+}
+closeNav();
+
 //Fixed Nav
 window.addEventListener("scroll", () => {
   var header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+// GSAP
+TweenMax.from(".h1", 2, {
+  delay: 0.5,
+  opacity: 0,
+  y: 30,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".p1", 2, {
+  delay: 0.7,
+  opacity: 0,
+  y: 20,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".a1", 2, {
+  delay: 0.9,
+  opacity: 0,
+  y: 20,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".logo", 2, {
+  delay: 0.2,
+  opacity: 0,
+  y: -20,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".nav", 2, {
+  delay: 0.3,
+  opacity: 0,
+  y: -20,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".circleOne", 2, {
+  delay: 0.4,
+  opacity: 0,
+  ease: Expo.easeInOut
+});
+
+TweenMax.from(".circleTwo", 2, {
+  delay: 0.4,
+  opacity: 0,
+  ease: Expo.easeInOut
 });
