78988081e7b94258d090df981d1cfdf225962419
Bruno Ascenção <brunoascenc1@outlook.com>
2021-04-28 14:51:05 -0300

    Add files via upload

---
+++ jest/script2.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch');
+
+
+const getPeople = fetch => {
+    return fetch('https://swapi.py4e.com/api/people/')
+        .then(response => response.json())
+        .then(data => {
+            return{
+                count: data.count,
+                results: data.results
+            }
+        })
+}
