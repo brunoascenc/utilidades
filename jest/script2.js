1a49bde7f61364743884946255db91b1eeaea775
Bruno Ascenção <brunoascenc1@outlook.com>
2021-04-29 16:20:24 -0300

    async tests

--- jest/script2.js
+++ jest/script2.js
@@ -1,13 +1,30 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
+const getPeoplePromise = (fetch) => {
+  return fetch("https://swapi.py4e.com/api/people")
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      return {
+        count: data.count,
+        results: data.results
+      };
+    });
+};
 
-const getPeople = fetch => {
-    return fetch('https://swapi.py4e.com/api/people/')
-        .then(response => response.json())
-        .then(data => {
-            return{
-                count: data.count,
-                results: data.results
-            }
-        })
-}
+const getPeople = async (fetch) => {
+  const getRequest = await fetch("https://swapi.py4e.com/api/people");
+  const data = await getRequest.json();
+  // console.log(data);
+  return {
+    count: data.count,
+    results: data.results
+  };
+};
+
+// getPeople(fetch);
+
+module.exports = {
+  getPeople,
+  getPeoplePromise,
+};
