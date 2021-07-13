2527e0c191b7b13784da8ac54cfcf67870f25f5e
brunoascenc <brunoascenc1@outlook.com>
2021-07-13 13:23:40 -0300

    working with dom elements

--- typescript/dist/main.js
+++ typescript/dist/main.js
@@ -1,14 +1,5 @@
-var doSomething = function () {
-    // return something
-    console.log('doSomething');
-};
-var doSomething2 = function () {
-    //never stops
-    throw 'never';
-};
-var vAny = 10;
-var vUnknown = 10;
-// let s1: string = vAny // can
-var s2 = vUnknown; //cant
-var pageNumber = '1';
-var numericPageNumber = pageNumber; // assign
+var someElement = document.querySelector('.foo');
+someElement.addEventListener('blur', function (event) {
+    var target = event.target; //as is the key
+    console.log('event', target.value);
+});
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,18 +1,6 @@
-const doSomething = (): void => {
-  // return something
-  console.log('doSomething');
-};
+const someElement = document.querySelector('.foo');
 
-const doSomething2 = (): never => {
-  //never stops
-  throw 'never';
-};
-
-let vAny: any = 10;
-let vUnknown: unknown = 10;
-
-// let s1: string = vAny // can
-let s2: string = vUnknown as string; //cant
-
-let pageNumber: string = '1';
-let numericPageNumber: number = pageNumber as unknown as number; // assign
+someElement.addEventListener('blur', (event) => {
+  const target = event.target as HTMLInputElement; //as is the key
+  console.log('event', target.value);
+});
