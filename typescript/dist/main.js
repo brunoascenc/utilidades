0098ecea5f9182925cd09455b0502faaac70d036
brunoascenc <brunoascenc1@outlook.com>
2021-07-13 12:05:42 -0300

    type definitions

--- typescript/dist/main.js
+++ typescript/dist/main.js
@@ -1,2 +1,7 @@
-var a = "1";
-console.log('aaa', a);
+// var a = "1"
+// console.log('aaa', a)
+// let hello: string = 'world';
+var getFullName = function (name, surname) {
+    return name + '' + surname;
+};
+console.log(getFullName('asas', 'asasas'));
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,2 +1,10 @@
-var a = "1"
-console.log('aaa', a)
+// var a = "1"
+// console.log('aaa', a)
+
+// let hello: string = 'world';
+
+const getFullName = (name: string, surname: string): string => {
+  return name + '' + surname;
+};
+
+console.log(getFullName('asas', 'asasas'));
