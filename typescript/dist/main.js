c4ac60afc2dad6c3e98531dca0467fad91d714e5
brunoascenc <brunoascenc1@outlook.com>
2021-07-14 10:47:53 -0300

    classes

--- typescript/dist/main.js
+++ typescript/dist/main.js
@@ -1,5 +1,47 @@
-var someElement = document.querySelector('.foo');
-someElement.addEventListener('blur', function (event) {
-    var target = event.target; //as is the key
-    console.log('event', target.value);
-});
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var User = /** @class */ (function () {
+    function User(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.unchangebleName = firstName;
+    }
+    User.prototype.changeUnchangebleName = function () {
+        // this.unchangebleName = "foo"
+    };
+    User.prototype.getFullName = function () {
+        return this.firstName + '' + this.lastName;
+    };
+    User.maxAge = 50;
+    return User;
+}());
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Admin.prototype.setEditor = function (editor) {
+        this.editor = editor;
+    };
+    Admin.prototype.getEditor = function () {
+        return this.editor;
+    };
+    return Admin;
+}(User));
+var user = new User('Bruno', 'Ascenção');
+console.log(user);
+var admin = new Admin('Foo', 'Bar');
+console.log(admin.firstName);
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,6 +1,43 @@
-const someElement = document.querySelector('.foo');
+interface UserInterface {
+  getFullName(): string;
+}
 
-someElement.addEventListener('blur', (event) => {
-  const target = event.target as HTMLInputElement; //as is the key
-  console.log('event', target.value);
-});
+class User implements UserInterface {
+  firstName: string;
+  lastName: string;
+  readonly unchangebleName: string;
+  static readonly maxAge = 50;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.unchangebleName = firstName;
+  }
+
+  changeUnchangebleName(): void {
+    // this.unchangebleName = "foo"
+  }
+
+  getFullName(): string {
+    return this.firstName + '' + this.lastName;
+  }
+}
+
+class Admin extends User {
+  //herança
+  private editor: string;
+
+  setEditor(editor: string): void {
+    this.editor = editor;
+  }
+
+  getEditor(): string {
+    return this.editor;
+  }
+}
+
+const user = new User('Bruno', 'Ascenção');
+console.log(user);
+
+const admin = new Admin('Foo', 'Bar');
+console.log(admin.firstName);
