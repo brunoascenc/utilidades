c4b2bf8de14db23ee8bb8ff0ff14c00e4eff0e79
brunoascenc <brunoascenc1@outlook.com>
2021-07-16 12:41:30 -0300

    usestate and useref

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import './App.css';
 import { TextField } from './TextField';
 
-
 const App: React.FC = () => {
   return (
     <div className="App">
-     <h1>a</h1>
-     <TextField text='oioi' person={{firstName: 'asas', lastName: 'as'}}/>
+      <h1>a</h1>
+      <TextField text="oioi" person={{ firstName: 'asas', lastName: 'as' }} />
     </div>
   );
-}
+};
 
 export default App;
--- typescript-react/src/TextField.tsx
+++ typescript-react/src/TextField.tsx
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useRef } from 'react';
 
 interface Person {
-    firstName: string
-    lastName: string
+  firstName: string;
+  lastName: string;
 }
 
 interface Props {
-    text?: string
-    ok?: boolean;
-    i?: number;
-    fn?: (bob: string) => void;
-    person?: Person;
+  text?: string;
+  ok?: boolean;
+  i?: number;
+  fn?: (bob: string) => void;
+  person?: Person;
+  handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const TextField: React.FC<Props> = ({person, text}) => {
-    return (
-        <div>
-            <input/>
-            {person?.firstName}
-            {person?.lastName}
-            {text}
-        </div>
-    )
+interface TextNode {
+  text: string;
 }
 
+export const TextField: React.FC<Props> = ({ handleChange }) => {
+  const [count, setCount] = useState<TextNode>({ text: 'hello' });
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div ref={divRef}>
+      <input ref={inputRef} onChange={handleChange} />
+    </div>
+  );
+};
