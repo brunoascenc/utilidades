c315aa1406adfe21cc84315e7890563faa966dfd
brunoascenc <brunoascenc1@outlook.com>
2021-07-16 13:08:47 -0300

    render props

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Counter } from './Counter';
 import { TextField } from './TextField';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>a</h1>
+      <Counter>
+        {({ count, setCount }) => (
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>count</button>
+          </div>
+        )}
+      </Counter>
       <TextField text="oioi" person={{ firstName: 'asas', lastName: 'as' }} />
     </div>
   );
