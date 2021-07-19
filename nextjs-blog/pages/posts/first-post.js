e03dbb82d66763119d42a221b7bb7abe60bc686c
brunoascenc <brunoascenc1@outlook.com>
2021-07-19 16:57:39 -0300

    head tag

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import Link from 'next/link';
+Head;
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>Fist Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
