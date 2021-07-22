214e964b0946c3a825c5636f24c706f2ada2c9ee
brunoascenc <brunoascenc1@outlook.com>
2021-07-22 12:49:52 -0300

    Passing props

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import List from './components/List';
 
 interface IState {
   people: {
@@ -20,6 +21,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <h1>People invited to my party</h1>
+      <List people={people} />
     </div>
   );
 };
