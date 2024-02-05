4d2c7d63ccea86ea3d2675d83409500266c73605
Bruno <bruno@i9tech.co>
2024-02-05 17:06:39 -0300

    c# quickstart

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        //static void PrintArray(int[] intArray, string message)
+        //{
+        //    foreach(int i in intArray)
+        //    {
+        //        Console.WriteLine("{0} : {1}", message, i);
+        //     }
+        //}
+
+        static void Main(string[] args)
+        {
+           //int[] favNums = new int[3];
+           //favNums[0] = 23;
+           //Console.WriteLine("favNum 0: {0}", favNums[0]);
+           //string[] customers = { "Bob", "Sally", "Sue" };
+           //var employees = new[] { "Mike", "Paul", "John Doe" };
+           //object[] randomArray = {"Paul", 45, 1.123};
+           //Console.WriteLine("randomArray 0: {0}", randomArray[0].GetType());
+           //Console.WriteLine("Array size: {0}", randomArray.Length);
+           //
+           //for (int i = 0; i < customers.Length; i++)
+           //{
+           //    Console.WriteLine("array: {0} : Value: {1}", i, customers[i]);
+           //}
+           //Console.WriteLine("---------------------------");
+           //string[,] custNames = new string[2, 2] { { "Bob", "AAAA" }, { "ASDSAD", "Smith" } };
+           //Console.WriteLine("Multidimensional value: {0}", custNames.GetValue(0, 1));
+           //for (int i = 0; i < custNames.GetLength(0); i++)
+           //{
+           //    for (int j = 0; j < custNames.GetLength(0); j++)
+           //    {
+           //        Console.WriteLine("{0} ", custNames[i, j]);
+           //    }
+           //}
+           //
+           //int[] randNums = { 1, 2, 3, 4, 5, 6 };
+           //PrintArray(randNums, "foreach");
+           //Console.WriteLine("---------------------------");
+           //
+           //Array.Sort(randNums);
+           //Array.Reverse(randNums);
+           //Console.WriteLine("1 at index: {0}", Array.IndexOf(randNums, 1));
+           //randNums.SetValue(0, 1);
+           //int[] srcArray = { 1, 2, 3 };
+           //int[] dstArray = new int[2];
+           //int startInd = 0;
+           //int length = 2;
+           //Array.Copy(srcArray, startInd, dstArray, 0, length);
+           //PrintArray(dstArray, "Copy");
+           //Array anotherArray = Array.CreateInstance(typeof(int), 10);
+           //srcArray.CopyTo(anotherArray, 5);
+           //
+           //foreach (int m in anotherArray)
+           //{
+           //    Console.WriteLine("Copy: {0}", m);
+           //}
+           //
+           //Console.WriteLine("---------------------------");
+
+             
+
+        }
+    }
+}
